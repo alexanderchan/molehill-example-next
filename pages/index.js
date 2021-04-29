ffad0b273c9c39d1dba22dec50610b82235bb41f
Alex Chan <alexander.m.chan@gmail.com>
2021-04-29 16:09:39 -0600

    testing setup

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,37 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { css } from '@emotion/css'
 import { mh } from '@molehill-ui/babel-plugin/macro'
+import { createCssTheme } from '@molehill-ui/theme'
+
+const {
+  config: { cssVars },
+} = createCssTheme()
+
+function Theme({ children }) {
+  return <div className={css(cssVars)}>{children}</div>
+}
 
 export default function Home() {
   const active = true
   return (
-    <div className={styles.container}>
-      <div
-        className={css(
-          mh({
-            border: '1px solid',
-            borderColor: 'rebeccapurple',
-            // need to fix ternaries to know parent property name
-            // mx: active ? '1' : '2',
-            p: '1',
-            color: 'blue-100',
-          })
-        )}
-      >
-        test
+    <Theme>
+      <div className={styles.container}>
+        <div
+          className={css(
+            mh({
+              border: '1px solid',
+              // need to fix ternaries to know parent property name
+              // mx: active ? '1' : '2',
+              p: '3',
+              color: 'white',
+              backgroundColor: 'blue-500',
+              borderRadius: 'md',
+            })
+          )}
+        >
+          test
+        </div>
       </div>
-    </div>
+    </Theme>
   )
 }
