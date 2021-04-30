6266ae187aacd828ea9924a690c164289bc11c30
Alex Chan <alexander.m.chan@gmail.com>
2021-04-30 13:59:43 -0600

    remove macro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,12 @@
 /// <reference types="@emotion/react/types/css-prop" />
-
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { css } from '@emotion/css'
-import { mh } from '@molehill-ui/babel-plugin/macro'
 import { createCssTheme } from '@molehill-ui/theme'
 
 const { cssVars } = createCssTheme()
 
 function Theme({ children }) {
   return <div className={css(cssVars)}>{children}</div>
-  // return <div style={{...cssVars}}>{children}</div>
 }
 
 export default function Home() {
@@ -19,41 +14,31 @@ export default function Home() {
     <Theme>
       <div className={styles.container}>
         <div
-          className={css(
-            mh({
-              display: 'flex',
-
-              mx: false ? '1' : 2,
-              p: '3',
-              px: 4,
-
-              border: '1px solid --color-blue-100',
-              // borderColor: 'blue-100',
-              color: 'white',
-              background: 'gray-600',
-              borderRadius: 'md',
-              boxShadow: 'lg',
-              fontWeight: 'bold',
-              '@media:sm': {
-                background: 'gray-600',
-              },
-              md: {
-                backgroundImage:
-                  'linear-gradient(to right, --color-blue-400, --color-indigo-800)',
-              },
-              '@media:xl': {
-                backgroundImage:
-                  'linear-gradient(to right, --color-red-400, --color-indigo-800)',
-              },
-            })
-          )}
-        >
-          This is a test
-        </div>
-        <div
           css={{
-            color: 'blue-600',
-            p: '4',
+            display: 'flex',
+
+            mx: false ? '1' : 2,
+            p: '3',
+            px: 4,
+
+            border: '1px solid --color-blue-100',
+            // borderColor: 'blue-100',
+            color: 'white',
+            background: 'gray-600',
+            borderRadius: 'md',
+            boxShadow: 'lg',
+            fontWeight: 'bold',
+            '@media:sm': {
+              background: 'gray-600',
+            },
+            md: {
+              backgroundImage:
+                'linear-gradient(to right, --color-blue-400, --color-indigo-800)',
+            },
+            '@media:xl': {
+              backgroundImage:
+                'linear-gradient(to right, --color-red-400, --color-indigo-800)',
+            },
           }}
         >
           emotion time
